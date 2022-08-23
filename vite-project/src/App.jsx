62abe95b77dcd02cb4c5fe92de56b99a4d17f566
Arka Sourav Karmakar <arka@Arkas-MacBook-Pro.local>
2022-08-23 20:33:25 +0530

    added Card Component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,22 @@
 import "./App.css";
 
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Card from "./components/Card";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
+      <Card
+        img="https://i.picsum.photos/id/668/200/300.jpg?hmac=E7YE9NQG89nCsmW1hc-1nACBZTj9ll8IiXS65WjdD28"
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        title="Life Lessons with Katie Zaferes"
+        price={136}
+      />
     </div>
   );
 }
---
+++ vite-project/src/components/Card.jsx
@@ -0,0 +1,18 @@
+function Card(props) {
+  return (
+    <div>
+      <img src={props.img} />
+      <div className="card--stats">
+        <span>{props.rating}</span>
+        <span className="gray">({props.reviewCount}) • </span>
+        <span className="gray">{props.country}</span>
+      </div>
+      <p>{props.title}</p>
+      <p>
+        <span className="bold">From ${props.price}</span> / person
+      </p>
+    </div>
+  );
+}
+
+export default Card;
