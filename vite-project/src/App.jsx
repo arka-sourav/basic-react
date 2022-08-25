fa9f3fc85216f2b54b7c2f1ad603aebc12c1389d
Arka Sourav Karmakar <arka@Arkas-MacBook-Pro.local>
2022-08-25 18:05:04 +0530

    added form component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 import Header from "./components/Header";
 import Meme from "./components/Meme";
 
+// import Form from "./components/Form";
+
 function App() {
   return (
     <div className="App">
